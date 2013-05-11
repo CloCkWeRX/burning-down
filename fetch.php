a26e22107336a329d2f4b5edd15637aae77c043a
Daniel O'Connor <daniel.oconnor@gmail.com>
2013-05-11 23:52:51 +0930

    Fetch from the overpass API a list of landuse or buildings within a 500m bounding box of a given fire.

--- fetch.php
+++ fetch.php
@@ -110,3 +110,268 @@ foreach ($files as $feed_file => $parser) {
   $fires += $engine->parse();
 }
 file_put_contents('output.json', json_encode($fires));
+
+require_once 'HTTP/Request2.php';
+require_once 'Cache/Lite.php';
+require_once 'Log.php';
+// Set a id for this cache
+
+
+// Set a few options
+$options = array(
+    'cacheDir' => 'cache/',
+    'lifeTime' => 3600
+);
+
+// Create a Cache_Lite object
+$cache = new Cache_Lite($options);
+
+
+$request = new HTTP_Request2('http://overpass-api.de/api/interpreter?data=[out:json];out;');
+$request->setMethod('POST');
+
+// 500m
+// 1 deg latitude = approx 111km
+// 500/111000 = 0.0045045045
+$offset = 0.0045045045;
+
+$log = new Log('');
+
+foreach ($fires as $fire) {
+
+  $query = simplexml_load_file('osm.xml');
+  $bbox = $query->xpath('//bbox-query');
+  //s="41.88917978666802" w="12.48945951461792" n="41.89152790667126" e="12.497506141662598"
+  //s="-35.24786215739992" w="137.67791748046875" n="-34.58573628651287" e="139.73785400390625
+
+  foreach ($bbox as $box) {
+    $box['n'] = $fire->lat + $offset;
+    $box['s'] = $fire->lat - $offset;
+    $box['w'] = $fire->long - $offset;
+    $box['e'] = $fire->long + $offset;
+
+  }
+
+  $id = md5($query->asXML());
+  $body = $cache->get($id);
+  if (!$body) {
+    $log->debug("Fetching");
+    $request->addPostParameter('data', $query->asXML());
+    $response = $request->send();
+
+    $body = $response->getBody();
+  
+    $cache->save($response->getBody(), $id);
+  } else {
+    $log->debug("Hit cache");
+  }
+  
+  $results = simplexml_load_string($body);
+  
+  $results->node()
+/*
+    [node] => Array
+        (
+            [0] => SimpleXMLElement Object
+                (
+                    [@attributes] => Array
+                        (
+                            [id] => 2017156745
+                            [lat] => -27.4482671
+                            [lon] => 151.9883475
+                        )
+
+                )
+
+            [1] => SimpleXMLElement Object
+                (
+                    [@attributes] => Array
+                        (
+                            [id] => 2017156746
+                            [lat] => -27.4386132
+                            [lon] => 151.9853201
+                        )
+
+                )
+
+            [2] => SimpleXMLElement Object
+                (
+                    [@attributes] => Array
+                        (
+                            [id] => 2017156758
+                            [lat] => -27.4492538
+                            [lon] => 151.9829314
+                        )
+
+                )
+
+            [3] => SimpleXMLElement Object
+                (
+                    [@attributes] => Array
+                        (
+                            [id] => 2017156766
+                            [lat] => -27.4368988
+                            [lon] => 151.9728899
+                        )
+
+                )
+
+            [4] => SimpleXMLElement Object
+                (
+                    [@attributes] => Array
+                        (
+                            [id] => 2017156768
+                            [lat] => -27.4392336
+                            [lon] => 151.9898184
+                        )
+
+                )
+
+            [5] => SimpleXMLElement Object
+                (
+                    [@attributes] => Array
+                        (
+                            [id] => 2017156769
+                            [lat] => -27.4498671
+                            [lon] => 151.9878448
+                        )
+
+                )
+
+            [6] => SimpleXMLElement Object
+                (
+                    [@attributes] => Array
+                        (
+                            [id] => 2017156773
+                            [lat] => -27.4477915
+                            [lon] => 151.9712162
+                        )
+
+                )
+
+        )
+
+    [way] => SimpleXMLElement Object
+        (
+            [@attributes] => Array
+                (
+                    [id] => 191137738
+                )
+
+            [nd] => Array
+                (
+                    [0] => SimpleXMLElement Object
+                        (
+                            [@attributes] => Array
+                                (
+                                    [ref] => 2017156773
+                                )
+
+                        )
+
+                    [1] => SimpleXMLElement Object
+                        (
+                            [@attributes] => Array
+                                (
+                                    [ref] => 2017156766
+                                )
+
+                        )
+
+                    [2] => SimpleXMLElement Object
+                        (
+                            [@attributes] => Array
+                                (
+                                    [ref] => 2017156746
+                                )
+
+                        )
+
+                    [3] => SimpleXMLElement Object
+                        (
+                            [@attributes] => Array
+                                (
+                                    [ref] => 2017156768
+                                )
+
+                        )
+
+                    [4] => SimpleXMLElement Object
+                        (
+                            [@attributes] => Array
+                                (
+                                    [ref] => 2017156745
+                                )
+
+                        )
+
+                    [5] => SimpleXMLElement Object
+                        (
+                            [@attributes] => Array
+                                (
+                                    [ref] => 2017156769
+                                )
+
+                        )
+
+                    [6] => SimpleXMLElement Object
+                        (
+                            [@attributes] => Array
+                                (
+                                    [ref] => 2017156758
+                                )
+
+                        )
+
+                    [7] => SimpleXMLElement Object
+                        (
+                            [@attributes] => Array
+                                (
+                                    [ref] => 2017156773
+                                )
+
+                        )
+
+                )
+
+            [tag] => Array
+                (
+                    [0] => SimpleXMLElement Object
+                        (
+                            [@attributes] => Array
+                                (
+                                    [k] => landuse
+                                    [v] => military
+                                )
+
+                        )
+
+                    [1] => SimpleXMLElement Object
+                        (
+                            [@attributes] => Array
+                                (
+                                    [k] => name
+                                    [v] => Borneo Barracks
+                                )
+
+                        )
+
+                    [2] => SimpleXMLElement Object
+                        (
+                            [@attributes] => Array
+                                (
+                                    [k] => operator
+                                    [v] => Australian Army
+                                )
+
+                        )
+
+                )
+
+        )
+
+
+*/
+
+  $fires += $engine->parse();
+}
