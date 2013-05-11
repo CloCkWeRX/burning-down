c9266825017ab993b43a87d8250005ebb8b1d8e4
Daniel O'Connor <daniel.oconnor@gmail.com>
2013-05-11 22:27:10 +0930

    Shift

--- data/cron.sh
+++ data/cron.sh
@@ -1,8 +1,8 @@
 
-wget -q -r http://www.ruralfire.qld.gov.au/bushfirealert/bushfireAlert.xml
-wget -q -r http://www.esa.act.gov.au/feeds/currentincidents.xml
-wget -q -r http://www.rfs.nsw.gov.au/feeds/majorIncidents.xml
-wget -q -r http://www.cfs.sa.gov.au/custom/criimson/CFS_Current_Incidents.xml
-wget -q -r http://osom.cfa.vic.gov.au/public/osom/IN_COMING.rss
-wget -q -r http://www.fire.tas.gov.au/Show?pageId=colBushfireSummariesRss
-wget -q -r http://sentinel.ga.gov.au/RSS/sentinelrss.xml
+wget -q -r http://www.ruralfire.qld.gov.au/bushfirealert/bushfireAlert.xml data/
+wget -q -r http://www.esa.act.gov.au/feeds/currentincidents.xml data/
+wget -q -r http://www.rfs.nsw.gov.au/feeds/majorIncidents.xml data/
+wget -q -r http://www.cfs.sa.gov.au/custom/criimson/CFS_Current_Incidents.xml data/
+wget -q -r http://osom.cfa.vic.gov.au/public/osom/IN_COMING.rss data/
+wget -q -r http://www.fire.tas.gov.au/Show?pageId=colBushfireSummariesRss data/
+wget -q -r http://sentinel.ga.gov.au/RSS/sentinelrss.xml data/
