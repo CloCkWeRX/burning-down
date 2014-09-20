2f3007068eb099531326f84b8ee2b3c8cd8a2ed3
Daniel O'Connor <daniel.oconnor@gmail.com>
2014-09-20 22:32:17 +1000

    Index complaints

--- lib/GeoRSSParser.php
+++ lib/GeoRSSParser.php
@@ -46,12 +46,18 @@ class DESQLDParser extends GeoRSSParser {
     $parts = explode(". ", $description);
 
     $loc = explode(": ", $parts[2]);
-    $fire->location = $loc[1];
+    if (!empty($loc)) {
+      $fire->location = $loc[1];
+    }
     $status = explode(": ", $parts[3]);
 
-    $fire->status = $status[1];
+    if (!empty($status)) {
+      $fire->status = $status[1];
+    }
     $desc = explode(": ", $parts[4]);
-    $fire->description = $desc[1];   
+    if (!empty($desc)) {
+      $fire->description = $desc[1];   
+    }
   }
 
 }
